0964361abdfddcd0d7b74d12a39da2cb1c911ec8
Matthew Kelliher <matthewkelliher@Matthews-MacBook-Air.local>
2015-11-04 16:13:57 -0600

    Deploy script for uploading to all nodes

---
+++ config/deploy/deploy.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# get the directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+# RSA key
+RSA_ID="${DIR}/id_geni_ssh_rsa"
+
+# node names
+VM1="mkellihe@pcvm3-3.instageni.utc.edu"
+VM2="mkellihe@pcvm3-4.instageni.utc.edu"
+VM3="mkellihe@pcvm3-5.instageni.utc.edu"
+VM4="mkellihe@pcvm3-6.instageni.utc.edu"
+VM5="mkellihe@pcvm3-7.instageni.utc.edu"
+VM6="mkellihe@pcvm3-8.instageni.utc.edu"
+VM7="mkellihe@pcvm3-9.instageni.utc.edu"
+VM8="mkellihe@pcvm3-10.instageni.utc.edu"
+
+
+
+echo "UPLOADING SERVER FILES..."
+
+for f in $( ls $DIR/servers/ ); do
+    sudo scp -i $RSA_ID $DIR/servers/$f $VM1:~/
+    sudo scp -i $RSA_ID $DIR/servers/$f $VM2:~/
+    sudo scp -i $RSA_ID $DIR/servers/$f $VM3:~/
+    sudo scp -i $RSA_ID $DIR/servers/$f $VM4:~/
+done
+
+echo "UPLOADING CLIENT FILES..."
+
+for f in $( ls $DIR/client/ ); do
+    sudo scp -i $RSA_ID $DIR/client/$f $VM8:~/
+done
+
+
+echo "UPLOADING COMMON FILES..."
+
+for f in $( ls $DIR/common/ ); do
+    sudo scp -i $RSA_ID $DIR/common/$f $VM1:~/
+    sudo scp -i $RSA_ID $DIR/common/$f $VM2:~/
+    sudo scp -i $RSA_ID $DIR/common/$f $VM3:~/
+    sudo scp -i $RSA_ID $DIR/common/$f $VM4:~/
+    sudo scp -i $RSA_ID $DIR/common/$f $VM8:~/
+done
+
+
+
+echo "DONE"
