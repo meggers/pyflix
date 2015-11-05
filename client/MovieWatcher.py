fa12dbf0f358dad47418a062923614be9d890f42
Max Eggers <eggers.max@gmail.com>
2015-11-04 20:31:26 -0600

    removed wait between frames

--- client/MovieWatcher.py
+++ client/MovieWatcher.py
@@ -10,19 +10,16 @@ class MovieWatcher(threading.Thread):
         threading.Thread.__init__ (self)
     
     def run(self):
+        start = time.time()
+        last_time = 0
         while True:
-            if self.empty_count >= 5000:
+            if self.empty_count >= 50000:
                 print "No frames received in the five seconds. Exiting Movie Watcher."
                 break
 
             frame = self.frame_buffer.get_frame()
             if frame is not None:
-                frame_no, data = frame
-                if frame_no % 1000 == 0:
-                    print frame_no
-                    
-                self.empty_count = 0;
-                time.sleep(0.01)
+                self.empty_count = 0
             else:
                 self.empty_count += 1
-                time.sleep(0.001)
+                time.sleep(0.0001)
