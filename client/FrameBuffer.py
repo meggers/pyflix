bb06133b6985bf82a8b27e4c8e65d7bd06af7ef3
Max Eggers <eggers.max@gmail.com>
2015-11-04 20:12:28 -0600

    stupid fix

--- client/FrameBuffer.py
+++ client/FrameBuffer.py
@@ -6,25 +6,17 @@ class FrameBuffer:
 
     def __init__(self, length):
         self.length = length
-        self.last_frame_no = -1
-        self.buffer = Queue.Queue(self.length)
+        self.buffer = Queue.PriorityQueue(self.length)
 
     def add_frame(self, frame_number, frame):
-        if not self.buffer.full() and self.last_frame_no + 1 == frame_number:
+        if not self.buffer.full():
             self.buffer.put((frame_number, frame))
-            self.last_frame_no = frame_number
             return True
         else:
             return False
 
     def get_frame(self):
         if not self.buffer.empty():
-            if self.buffer.qsize() == 1:
-                self.last_frame_no = -1
-
             return self.buffer.get()
         else:
             return None
-
-    def last_frame_added(self):
-        return self.last_frame_no
