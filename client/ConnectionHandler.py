07563fa7574d75dabccb9a408a3c53f9442b784e
Matthew Kelliher <matthewkelliher@dyn-72-33-171-155.uwnet.wisc.edu>
2015-12-02 19:20:01 -0600

    Client stuff

--- client/ConnectionHandler.py
+++ client/ConnectionHandler.py
@@ -1,37 +1,146 @@
 #!/usr/bin/python
 
-import json, threading, SocketServer
+from socket import *
+from sys import *
+import threading, time
 
-class DataHandler(SocketServer.StreamRequestHandler):
 
-    def handle(self):
-        response_message = self.request[0][1:-1]
-        socket = self.request[1]
-
-        frame_no = int(response_message[:5])
-
-        if self.server.data_buffer.add_frame(frame_no, response_message):
-            request_frame = frame_no + 4
+class ServerManager():
+    def __init__(self, servers, frame_buffer):
+        self.current_frame = -1
+        self.servers = servers
+        self.frame_buffer = frame_buffer
+        
+    def start(self):
+        port = 5005
+        
+        self.cons = [ServerConnection(ip, port, i * 8) for i, ip in enumerate(self.servers)]
+        
+        for ip, server in self.cons.iteritems():
+            server.start()
+        
+        self.frame_queue.listen()
+    
+    def window_complete(self, id):
+        print "COMPLETE: {}, t = {}".format(id, self.cons[id].delay)
+        
+        self.configs[id]['frm'] += self.configs[id]['wnd'] * self.configs[id]['del']
+        
+        if self.all_windows_done():
+            self.reconfigure()
+        
+        if self.configs[id]['frm'] > 200:
+            self.frame_queue.close()
         else:
-            request_frame = frame_no
+            self.cons[id].start(self.configs[id])
+            
+            
+    def all_windows_done(self):
+        for id in xrange(0,4):
+            if self.cons[id].receiving:
+                return False
+        return True
+        
+    def reconfigure(self):
+        delays = [
+            self.cons[0].delay,
+            self.cons[1].delay,
+            self.cons[2].delay,
+            self.cons[3].delay
+        ]
+        
+        
+        
+        
+    def handle_frame(self, id, data):
+        if len(data) == 0:
+            self.window_complete(id)
+        elif data == "ACK":
+            print "Recvd ACK"
+        else:
+            frame_num = int(data[:5])
+            print "Recvd frame: {}, length: {}".format(frame_num, len(data))
+            
+            
+    # class FrameQueue():
+    
+    #     def __init__(self):
+    #         self.q = Queue.Queue()
+            
+    #     def put(self, id, data):
+    #         self.q.put((id, data))
+            
+    #     def get(self):
+    #         return self.q.get()
+            
+    #     def close(self):
+    #         self.done = True
+            
+    #     def listen(self):
+    #         self.done = False
+    #         while not self.done:
+    #             id, data = self.get()
+    #             servers.handle_frame(id, data)
+                
 
-        request = {"frm": request_frame}
-
-        socket.sendto(json.dumps(request), self.client_address)
-
-class ConnectionHandler(threading.Thread):
-
-    def __init__(self, hostname, port, server_ips, data_buffer):
+        
+class ServerConnection():
+    
+    def __init__(self, host, port, frame, window = 8):
+        self.host = host
         self.port = port
-        self.server_ips = server_ips
-        self.server = SocketServer.UDPServer((hostname, port), DataHandler)
-        self.server.data_buffer = data_buffer
-        threading.Thread.__init__ (self)
+        self.frame = frame
+        self.window = window
+        
+        self.receiving = False
+        self.delay = -1
+        
+    def tick(self):
+        self.tick_time = time.time()
+        
+    def tock(self):
+        return time.time() - self.tick_time
+        
+    def start(self):
+        
+        request = {"cmd": "Start", "frm": self.frame, "wnd": self.window}
+        
+        self.tick()
+        self.receiving = True
+        self.thread = threading.Thread(target=ServerConnection.threaded_send_request, args=(self, request)).start()
+    
+    def window_received(self):
+        self.delay = self.tock()
+        self.receiving = False
+    
+    @staticmethod
+    def threaded_send_request(server, request):
+        
+        # Create a socket (SOCK_STREAM means a TCP socket)
+        sock = socket(AF_INET, SOCK_STREAM)
+        # Connect to server
+        sock.connect((server.host, server.port))
+        
+        # Send data
+        sock.sendall(json.dumps(request)+"\n")
+        try:
+            done = False
+            while not done:
+                # Receive data from the server
+                data = sock.recv(1030)
+                
+                if len(data) == 0:
+                    done = True
+                
+                servers.frame_queue.put(server.host, data + "")
+                
+        finally:
+            sock.close()
+            
+        server.window_received()
 
-    def run(self):
-        print "Starting movie stream..."
-        for index, ip in enumerate(self.server_ips):
-            self.server.socket.sendto(json.dumps({"frm": index}), (ip, self.port))
-
-        print "Listening on port..."
-        self.server.serve_forever()
+    
+    
+    
+    
+    
--- client/client.py
+++ client/client.py
@@ -9,14 +9,14 @@ UDP_HOSTNAME = "0.0.0.0"
 UDP_PORT = 5000
 
 # server list
-servers = [ "10.0.0.0" ]#"vm1" , "vm2", "vm3", "vm4"]
+servers = ["vm1" , "vm2", "vm3", "vm4"]
 
 def main():
     global UDP_HOSTNAME, UDP_PORT, servers
 
     frame_buffer = FrameBuffer(32)
 
-    movie_stream = ConnectionHandler(UDP_HOSTNAME, UDP_PORT, servers, frame_buffer)
+    movie_stream = ServerManager(servers, frame_buffer)
     movie_stream.start()
 
     movie_thread = MovieWatcher(frame_buffer)
